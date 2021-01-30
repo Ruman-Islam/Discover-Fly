0f96937e55b6bd1d5e6e11116446125a60234caa
Ruman-Islam <rumanislam48@gmail.com>
2021-01-30 19:56:19 +0600

    invoce updated

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ function invoice(ticket, invo, none, block) {
     const totalPrice = document.getElementById("total").innerText;
     const priceFloat = parseFloat(totalPrice);
     document.getElementById("invo-total").innerText = priceFloat;
-    const firstTicketInput = document.getElementById(ticket + "-class");
-    const firstQuantity = parseFloat(firstTicketInput.value);
-    document.getElementById("invo-" + invo).innerText = firstQuantity;
-    return firstQuantity;
+    const TicketInput = document.getElementById(ticket + "-class");
+    const Quantity = parseFloat(TicketInput.value);
+    document.getElementById("invo-" + invo).innerText = Quantity;
+    return Quantity;
 }
 
 // Thank You
